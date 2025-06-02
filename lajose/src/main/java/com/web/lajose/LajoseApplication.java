22fe89eebd4375efdab53af6497085bbc6328984
Caleb <127038749+calebsitox@users.noreply.github.com>
2025-06-02 21:35:02 +0200

    First Commit

---
+++ lajose/src/main/java/com/web/lajose/LajoseApplication.java
@@ -0,0 +1,13 @@
+package com.web.lajose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LajoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LajoseApplication.class, args);
+	}
+
+}
